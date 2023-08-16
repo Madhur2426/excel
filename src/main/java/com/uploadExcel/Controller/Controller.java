802e8990e180ff8a672bbbe507f8d24ee07fc376
Madhur2426 <64002982+Madhur2426@users.noreply.github.com>
2023-08-16 16:24:36 +0530

    Update Controller.java

--- src/main/java/com/uploadExcel/Controller/Controller.java
+++ src/main/java/com/uploadExcel/Controller/Controller.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.uploadExcel.Entity.ExcelTable;
 import com.uploadExcel.Service.UploadImplementation;
 @CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://madhur2426.github.io")
 @RestController
 public class Controller {
 	@Autowired
